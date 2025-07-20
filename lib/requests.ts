04fcb9dd4b402d009ae0e5d11581d60a8bb45c80
Contrapunctus-XIV <contrapunctusxiv@proton.me>
2025-07-20 18:09:14 +0200

    refonte requests

--- lib/requests.ts
+++ lib/requests.ts
@@ -4,14 +4,11 @@
 
 import crypto from 'node:crypto';
 import { JvcResponseError } from './errors.js';
-import { API_DOMAIN, API_VERSION, HTTP_CODES, MAX_REQUEST_RETRIES, SECOND_DELAY, USER_AGENT } from './vars.js';
+import { API_DOMAIN, API_VERSION, HTTP_CODES, DEFAULT_REQUEST_RETRIES, SECOND_DELAY, USER_AGENT, PARTNER_KEY, HMAC_SECRET } from './vars.js';
 import { sleep } from './utils.js';
 import util from "node:util";
 import { exec as srcExec } from "node:child_process";
 import { LibTypes } from './types/index.js';
-
-const PARTNER_KEY = '550c04bf5cb2b';
-const HMAC_SECRET = 'd84e9e5f191ea4ffc39c22d11c77dd6c';
 
 function normalizeHeaders(headers: Record<string, string>) {
     for (const header in headers) {
@@ -24,7 +21,7 @@ function normalizeHeaders(headers: Record<string, string>) {
             const value = headers[header];
             delete headers[header];
 
-            headers[capitalizedHeader] = value.toLowerCase();
+            headers[capitalizedHeader] = value;
         }
     }
 
@@ -60,7 +57,7 @@ function authHeader(path: string, method: string = 'GET', query: { [key: string]
 }
 
 
-function buildDataAsParams(data: Record<string, any>): string {
+function objectToParams(data: Record<string, any>): URLSearchParams {
     const params = new URLSearchParams();
     for (const [key, value] of Object.entries(data)) {
         if (Array.isArray(value)) {
@@ -70,7 +67,49 @@ function buildDataAsParams(data: Record<string, any>): string {
         }
     }
 
-    return params.toString();
+    return params;
+}
+
+function objectToFormData(data: Record<string,any>): FormData {
+    const formData = new FormData();
+
+    for (const [k,v] of Object.entries(data)) {
+        if (v instanceof Array) {
+            for (const el of v) {
+                formData.append(k, el);
+            }
+        } else {
+            formData.append(k, v.toString());
+        }
+    }
+
+    return formData;
+}
+
+function parseData(data: any, method: LibTypes.Requests.HttpMethod, bodyMode: LibTypes.Requests.BodyMode): LibTypes.Requests.BodyType {
+    if (['GET', 'HEAD'].includes(method.toUpperCase())) {
+        return undefined;
+    }
+
+    let parsedData = undefined;
+
+
+    switch (bodyMode) {
+        case "json":
+            parsedData = JSON.stringify(data);
+            break;
+        case "url":
+            parsedData = objectToParams(data);
+            break;
+        case "form":
+            parsedData = objectToFormData(data);
+            break;
+        case "any":
+            parsedData = data;
+            break;
+    }
+
+    return parsedData;
 }
 
 /**
@@ -87,64 +126,41 @@ function buildDataAsParams(data: Record<string, any>): string {
  * @throws {@link errors.JvcResponseError | JvcResponseError} si un statut HTTP signalant un échec a été rencontré et qu'il n'est pas listé dans `allowedStatusErrors`
  * 
  */
-export async function callApi(path: string, { method = 'GET', query = {}, data = undefined, cookies = {}, headers = {}, allowedStatusErrors = [HTTP_CODES.BAD_REQUEST, HTTP_CODES.NOT_FOUND] }: { method?: LibTypes.Requests.HttpMethod; query?: Record<string, any>; data?: any; cookies?: Record<string, string>; headers?: Record<string, string>; allowedStatusErrors?: number[] } = {}): Promise<Response> {
-    const url = new URL(`https://${API_DOMAIN}/v${API_VERSION}/${path}`);
-    if (Object.keys(query).length > 0) {
-        url.search = new URLSearchParams(query).toString();
-    }
 
-    headers = normalizeHeaders(headers);
-
+/**
+ * Effectue une requête à l'API `v4`, à l'endpoint et avec les options spécifiées, puis renvoie la réponse obtenue.
+ * 
+ * @param {string} path *endpoint* (chemin relatif) auquel adresser la requête. Exemple : `accounts/login`. La liste des *endpoints* est disponible sur [JVFlux](https://jvflux.fr/Documentation_de_l%27API_Jeuxvideo.com#API_v4).
+ * @param {LibTypes.Requests.Options} [options] options permettant de modifier le comportement de la requête
+ * @param {LibTypes.Requests.HttpMethod} [options.method] méthode HTTP de la requête (`"GET"` par défaut)
+ * @param {Record<string, any>} [options.query] les paramètres URL à passer à la requête, sous forme d'objet associant au paramètre sa valeur
+ * @param {LibTypes.Requests.BodyType} [options.data] le corps de la requête (pour méthodes `POST` et `PUT`). Doit être un objet sauf si `bodyMode` vaut `"any"`.
+ * @param {"json" | "url" | "form" | "any"} [options.bodyMode] format du corps de la requête : `"json"` pour envoyer un objet (par défaut, `Content-Type: application/json`), `"url"` pour envoyer un objet {@link !URLSearchParams | `URLSearchParams`} (`Content-Type: x-www-form-urlencoded`), `"form"` pour envoyer un objet {@link !FormData | `FormData`} (`Content-Type: multipart/form-data`), `"any"` pour envoyer n'importe quel type de variable. Si `"any"` n'est pas choisi, c'est un objet qui est attendu en tant qu'argument `data` et qui sera automatiquement converti dans le type souhaité avec ajout du *header* approprié. Pour `"any"`, la valeur de `data` sera envoyée telle quelle sans modification et le *header* `Content-Type` sera à renseigner par l'utilisateur
+ * @param {Record<string, string>} [options.cookies] les cookies à envoyer sous forme d'objet associant au nom du cookie sa valeur
+ * @param {Record<string, string>} [options.headers] les en-têtes à envoyer sous forme d'objet associant au nom de l'en-tête sa valeur
+ * @param {number} [options.retries] nombre maximal de tentatives d'envoi de la requête au cas où une erreur `429 Too Many Requests` se produit (par défaut `3`)
+ * @param {number} [options.retryDelay] délai entre chaque tentative en millisecondes (par défaut `1 000`)
+ * @param {number[]} [options.allowedStatusErrors] contient les statuts HTTP signalant un échec à ignorer, c'est-à-dire ceux qui ne causeront pas l'erreur {@link errors.JvcResponseError | JvcResponseError} si renvoyés. Contient par défaut les statuts 400 (`Bad Request`) et 404 (`Not Found`)
+ * @throws {@link errors.JvcResponseError | JvcResponseError} si un statut HTTP signalant un échec a été rencontré et qu'il n'est pas listé dans `allowedStatusErrors`
+ * 
+ * @returns 
+ */
+export async function requestApi(path: string, { method = 'GET', query = {}, data = undefined, cookies = {}, headers = {}, allowedStatusErrors = [HTTP_CODES.BAD_REQUEST, HTTP_CODES.NOT_FOUND], bodyMode = "json", retries = DEFAULT_REQUEST_RETRIES, retryDelay = SECOND_DELAY }: { method?: LibTypes.Requests.HttpMethod; query?: Record<string, any>; data?: LibTypes.Requests.BodyType; cookies?: Record<string, string>; headers?: Record<string, string>; allowedStatusErrors?: number[]; bodyMode?: LibTypes.Requests.BodyMode; retries?: number; retryDelay?: number } = {}): Promise<Response> {
+    const url = `https://${API_DOMAIN}/v${API_VERSION}/${path}`;
     const jvAuth = authHeader(path, method, query);
-    const reqHeaders = new Headers({
+    const reqHeaders = {
         ...headers,
         "Jvc-Authorization": jvAuth,
-        "Content-Type": headers['content-type'] || 'application/json',
         "Jvc-App-Platform": "Android",
         "Jvc-App-Version": "338",
         "User-Agent": "JeuxVideo-Android/338",
-        "Host": API_DOMAIN,
-        ...(cookies && { 'cookie': Object.entries(cookies).map(([key, value]) => `${key}=${value}`).join('; ') })
-    });
-
-    let parsedData = undefined;
-
-    if (!['GET', 'HEAD'].includes(method.toUpperCase())) {
-        if (reqHeaders.get('Content-Type') === 'application/json') {
-            parsedData = JSON.stringify(data);
-        } else if (reqHeaders.get('Content-Type') === 'application/x-www-form-urlencoded') {
-            parsedData = buildDataAsParams(data);
-        } else if (reqHeaders.get('Content-Type') === "multipart/form-data") {
-            parsedData = convertObjToFormData(data);
-        }
-        else {
-            parsedData = data;
-        }
-    }
-
-    const options: RequestInit = {
-        method,
-        headers: reqHeaders,
-        body: parsedData,
+        "Host": API_DOMAIN
     };
-
-    try {
-        const parsedUrl = url.toString();
-        const response = await fetch(parsedUrl, options);
-
-        if (!response.ok && !allowedStatusErrors.includes(response.status)) {
-            throw new JvcResponseError(`Unexpected status code signalling a request failure: ${response.status}.`);
-        }
-
-        return response;
-    } catch (error) {
-        throw error;
-    }
+    
+    return request(url, { method, query, headers: reqHeaders, data, cookies, allowedStatusErrors, bodyMode, curl: false, retries, retryDelay })
 }
 
 const exec = util.promisify(srcExec);
-
-
 const DELIMITER = "-".repeat(6);
 
 // associe à chaque attribut de RawResponse la variable "Write out" associée de cURL
@@ -174,7 +190,7 @@ const PARSE_RESULT_ATTRS: LibTypes.Requests.CurlParsers = {
     "url": (str: string) => str.trim()
 };
 
-function convertRequestIntoCurl(request: Request, { body = undefined }: { body?: string | FormData } = {}): string {
+function convertRequestIntoCurl(request: Request, body: LibTypes.Requests.BodyType): string {
     let command = `echo ${CURL_SCHEME["body"][0]} & `;
     // -X : méthode, -L : suivre redirections, -A : User-Agent, -s : pas de barre de progression
     command += `curl -s -X "${request.method}" -L "${request.url}" -A "${USER_AGENT}"`;
@@ -185,6 +201,9 @@ function convertRequestIntoCurl(request: Request, { body = undefined }: { body?:
             for (const [k,v] of body.entries()) {
                 command += ` -F "${k}=${v}"`;
             }
+            request.headers.set("Content-Type", "multipart/form-data");
+        } else if (body instanceof URLSearchParams) {
+            command += ` -d "${body.toString()}"`;
         } else {
             command += ` -d "${body}"`;
         }
@@ -237,95 +256,85 @@ function convertCurlIntoResponse(stdout: string): Response {
     return response;
 }
 
-function convertObjToFormData(data: Record<string,any>): FormData {
-    const formData = new FormData();
-
-    for (const [k,v] of Object.entries(data)) {
-        if (v instanceof Array) {
-            for (const el of v) {
-                formData.append(k, el);
-            }
-        } else {
-            formData.append(k, v.toString());
-        }
-    }
-
-    return formData;
-}
-
 /**
  * Effectue une requête à l'URL passée en entrée et avec les options spécifiées puis renvoie la réponse obtenue.
- * Utilise [`cURL`](https://curl.se/docs/manpage.html) afin de contourner les restrictions Cloudflare des serveurs JVC.
  * 
- * @param {string} url URL de la requête
+ * @param {string | URL} url URL de la requête
  * @param {LibTypes.Requests.Options} [options] options permettant de modifier le comportement de la requête
- * @param {LibTypes.Requests.HttpMethod} [options.method] méthode HTTP de la requête (`GET` par défaut)
+ * @param {LibTypes.Requests.HttpMethod} [options.method] méthode HTTP de la requête (`"GET"` par défaut)
  * @param {Record<string, any>} [options.query] les paramètres URL à passer à la requête, sous forme d'objet associant au paramètre sa valeur
- * @param {any} [options.data] le corps de la requête (pour méthodes `POST` et `PUT`). Son format est arbitraire est dépend du header `Content-Type` fourni : `application/json` (header par défaut), `application/x-www-form-urlencoded` et `multipart/form-data` recquièrent tous un objet. Pour tout autre header tel que `application/octet-stream`, la valeur de ce paramètre sera passée telle que donnée par l'utilisateur à la requête
+ * @param {any} [options.data] le corps de la requête (pour méthodes `POST` et `PUT`). Doit être un objet sauf si `bodyMode` vaut `"any"`.
+ * @param {"json" | "url" | "form" | "any"} [options.bodyMode] format du corps de la requête : `"json"` pour envoyer un objet (par défaut, `Content-Type: application/json`), `"url"` pour envoyer un objet {@link !URLSearchParams | `URLSearchParams`} (`Content-Type: x-www-form-urlencoded`), `"form"` pour envoyer un objet {@link !FormData | `FormData`} (`Content-Type: multipart/form-data`), `"any"` pour envoyer n'importe quel type de variable. Si `"any"` n'est pas choisi, c'est un objet qui est attendu en tant qu'argument `data` et qui sera automatiquement converti dans le type souhaité avec ajout du *header* approprié. Pour `"any"`, la valeur de `data` sera envoyée telle quelle sans modification et le *header* `Content-Type` sera à renseigner par l'utilisateur
  * @param {Record<string, string>} [options.cookies] les cookies à envoyer sous forme d'objet associant au nom du cookie sa valeur
- * @param {Record<string, string>} [options.headers] les en-têtes à envoyer sous forme d'objet associant au nom de l'en-tête sa valeur. Une en-tête particulièrement importante est `Content-Type` car elle déterminera la manière dont sera traitée le paramètre optionnel `data` (voir ci-dessus)
+ * @param {Record<string, string>} [options.headers] les en-têtes à envoyer sous forme d'objet associant au nom de l'en-tête sa valeur
+ * @param {number} [options.retries] nombre maximal de tentatives d'envoi de la requête au cas où une erreur `429 Too Many Requests` se produit (par défaut `3`)
+ * @param {number} [options.retryDelay] délai entre chaque tentative en millisecondes (par défaut `1 000`)
  * @param {number[]} [options.allowedStatusErrors] contient les statuts HTTP signalant un échec à ignorer, c'est-à-dire ceux qui ne causeront pas l'erreur {@link errors.JvcResponseError | JvcResponseError} si renvoyés. Contient par défaut les statuts 400 (`Bad Request`) et 404 (`Not Found`)
+ * @param {boolean} [options.curl] `true` pour utiliser [`cURL`](https://curl.se/docs/manpage.html) afin de contourner les restrictions Cloudflare des serveurs JVC (nécessite que `cURL` soit installé sur la machine), `false` pour envoyer la requête avec {@link !fetch | `fetch`} (par défaut)
  * @throws {@link errors.JvcResponseError | JvcResponseError} si un statut HTTP signalant un échec a été rencontré et qu'il n'est pas listé dans `allowedStatusErrors`
  * 
  * @returns {Promise<Response>}
  */
-export async function curl(url: string, { method = "GET", query = {}, headers = {}, data = undefined, cookies = {}, allowedStatusErrors = [HTTP_CODES.BAD_REQUEST, HTTP_CODES.NOT_FOUND] }: { method?: LibTypes.Requests.HttpMethod; query?: Record<string, any>; data?: any; cookies?: Record<string, string>; headers?: Record<string, string>; allowedStatusErrors?: number[] } = {}): Promise<Response> {
+export async function request(url: string | URL, { method = "GET", query = {}, headers = {}, data = undefined, cookies = {}, allowedStatusErrors = [HTTP_CODES.BAD_REQUEST, HTTP_CODES.NOT_FOUND], bodyMode = "json", curl = false, retries = DEFAULT_REQUEST_RETRIES, retryDelay = SECOND_DELAY }: { method?: LibTypes.Requests.HttpMethod; query?: Record<string, any>; data?: LibTypes.Requests.BodyType; cookies?: Record<string, string>; headers?: Record<string, string>; allowedStatusErrors?: number[]; bodyMode?: LibTypes.Requests.BodyMode; curl?: boolean; retries?: number; retryDelay?: number } = {}): Promise<Response> {
     headers = normalizeHeaders(headers);
 
     const reqHeaders = new Headers({
         ...headers,
-        "Content-Type": headers['Content-Type'] || 'application/json',
         ...(cookies && { 'Cookie': Object.entries(cookies).map(([key, value]) => `${key}=${value}`).join('; ') })
     });
 
-    const urlWithParams = new URL(url);
+    const urlObject = new URL(url);
     if (Object.keys(query).length > 0) {
-        urlWithParams.search = new URLSearchParams(query).toString();
+        urlObject.search = new URLSearchParams(query).toString();
     }
 
-    let parsedData: string | FormData | undefined = undefined;
+    let parsedData = parseData(data, method, bodyMode);
 
-    if (!['GET', 'HEAD'].includes(method.toUpperCase())) {
-        if (reqHeaders.get('Content-Type') === 'application/json') {
-            parsedData = JSON.stringify(data);
-        } else if (reqHeaders.get('Content-Type') === 'application/x-www-form-urlencoded') {
-            parsedData = buildDataAsParams(data);
-        } else if (reqHeaders.get('Content-Type') === "multipart/form-data") {
-            parsedData = convertObjToFormData(data);
-        }
-        else {
-            parsedData = data;
-        }
+    if (parsedData !== undefined && bodyMode === "json") {
+        reqHeaders.set("Content-Type", "application/json");
     }
 
-    const request: Request = new Request(urlWithParams, {
+    const options: RequestInit = {
         method,
-        headers: reqHeaders
-    });
+        headers: reqHeaders,
+        body: parsedData
+    };
 
-    const command = convertRequestIntoCurl(request, { body: parsedData });
-    let response: Response;
-    let retries = 0;
+    let response: Response = new Response();
+    let currentTry = 0;
+    while (currentTry < retries) {
+        currentTry += 1;
 
-    while (true) {
-        const { stdout, stderr } = await exec(command);
-        if (stderr) {
-            throw new Error(`Call to cURL failed: ${stderr}`);
+        if (curl) {
+            response = await curlRequest(urlObject, options);
+        } else {
+            response = await fetch(urlObject, options);
         }
 
-        response = convertCurlIntoResponse(stdout);
-
-        if (response.status !== HTTP_CODES.TOO_MANY_REQUESTS || retries > MAX_REQUEST_RETRIES) { // Too Many Request
+        if (response.status !== HTTP_CODES.TOO_MANY_REQUESTS || allowedStatusErrors.includes(HTTP_CODES.TOO_MANY_REQUESTS)) {
             break;
         }
 
-        await sleep(SECOND_DELAY)
-        retries += 1
+        await sleep(retryDelay);
     }
+
 
     if (!response.ok && !allowedStatusErrors.includes(response.status)) {
         throw new JvcResponseError(`Unexpected status code signalling a request failure: ${response.status}.`);
     }
 
+    return response;
+}
+
+async function curlRequest(url: URL, options: RequestInit): Promise<Response> {
+    const requestObject = new Request(url, options);
+    const command = convertRequestIntoCurl(requestObject, options.body);
+    
+    const { stdout, stderr } = await exec(command);
+    if (stderr) {
+        throw new Error(`Call to cURL failed: ${stderr}. Is cURL installed on the machine?`);
+    }
+    
+    const response = convertCurlIntoResponse(stdout);
     return response;
 }
